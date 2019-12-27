40440e052ce8eae20087deb766519112b17373c1
galileoluna <galileoungs@gmail.com>
2019-12-27 13:42:35 -0300

    genero barra navegacion y configuro la tarjeta de agregado

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../database')
+const pool = require('../database');
+router.get('/add', (req, res) => {
+    res.render('links/add');
+});
 
 module.exports = router;
